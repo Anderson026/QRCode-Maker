e6d9555bb767e25a46cf1e6c3b752a540e425030
Anderson026 <anderson-ferreira22@hotmail.com>
2021-11-21 15:04:22 -0300

    Adicionando um placeholder no input

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     <div className="App">
       <div className="container">
         <header><h1>QRCode Maker</h1></header>
-        <input type="url" onChange={(element) => {setText(element.target.value)}} value={text}></input>
+        <input type="url" placeholder="Digite ou cole o link que deseja gerar o QRCode" onChange={(element) => {setText(element.target.value)}} value={text}></input>
         <button onClick={clearInput}>Apagar Link</button>
         <QRCodeCanvas text={text}></QRCodeCanvas>
       </div>
